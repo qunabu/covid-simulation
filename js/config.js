cbd5e59b0b6de538a4ca73fa69a6b2390907ccbb
Mateusz <mateusz@qunabu.com>
2020-03-18 09:14:26 +0100

    progress

--- js/config.js
+++ js/config.js
@@ -1,15 +1,5 @@
 import * as dat from "./dat.gui.module.js";
 
-var FizzyText = function() {
-  this.message = "dat.gui";
-  this.speed = 0.8;
-  this.displayOutline = false;
-  this.explode = function() {
-    console.log("expolode");
-  };
-  // Define render logic ...
-};
-
 export const config = (config, onSubmit) => {
   const actions = {
     submit: onSubmit
@@ -18,8 +8,6 @@ export const config = (config, onSubmit) => {
   gui.add(config, "width", 500, 1500);
   gui.add(config, "height", 200, 1000);
   gui.add(config, "amount", 1, 1000);
-  //gui.add(text, "speed", -5, 5);
-  //gui.add(text, "displayOutline");
   gui.add(actions, "submit");
 };
 
